c5ae4a498bd3d28c6376401e424bd650075036af
Landon Bass <landon.bass@gmail.com>
2012-12-05 13:10:32 -0500

    changed object cache to output cache
    mental slip i guess...

--- verify-spobjectcachelimit.ps1
+++ verify-spobjectcachelimit.ps1
@@ -1,14 +1,14 @@
 #author: landon bass (and todd carter - http://todd-carter.com/post/2012/01/31/when-page-output-caching-does-not-output)
 #creates a sql statement to count uniquely secured items in a site collection
 #can also attempt to execute the script
-#if the result is greater than 10,000 then object caching will not work
+#if the result is greater than 10,000 then output caching will not work
 
 
 add-pssnapin microsoft.sharepoint.powershell -ea 0 
 cls
 $url = ""
 
-function verify-spobjectcachelimit (
+function verify-spoutputcachelimit (
     [parameter(mandatory=$true)]
     $url,
     [bool]$execute
@@ -41,4 +41,4 @@ function verify-spobjectcachelimit (
     }
 }
 
-verify-spobjectcachelimit -url $url -execute $false
+verify-spoutputcachelimit -url $url -execute $false
