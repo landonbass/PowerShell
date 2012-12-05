bc6facaf70a1ab9a0009b2f3c7db75d6c864c983
Landon Bass <landon.bass@gmail.com>
2012-12-05 13:07:53 -0500

    grammar correction ;)

--- verify-spobjectcachelimit.ps1
+++ verify-spobjectcachelimit.ps1
@@ -1,7 +1,7 @@
 #author: landon bass (and todd carter - http://todd-carter.com/post/2012/01/31/when-page-output-caching-does-not-output)
 #creates a sql statement to count uniquely secured items in a site collection
 #can also attempt to execute the script
-#if the result is greater then 10,000 then object caching will not work
+#if the result is greater than 10,000 then object caching will not work
 
 
 add-pssnapin microsoft.sharepoint.powershell -ea 0 
