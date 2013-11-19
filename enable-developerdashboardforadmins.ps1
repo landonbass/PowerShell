a47f58816924b532ccff0f67cab3c7612ac1c11a
Landon Bass <landon.bass@gmail.com>
2013-11-19 10:00:00 -0500

    Create enable-developerdashboardforadmins.ps1

---
+++ enable-developerdashboardforadmins.ps1
@@ -0,0 +1,11 @@
+#enable deveveloper dashboard for admins
+
+add-pssnapin microsoft.sharepoint.powershell -ea 0
+cls
+$svc = [microsoft.sharepoint.administration.spwebservice]::contentservice
+$dashboard = $svc.developerdashboardsettings
+$dashboard.requiredpermissions = [microsoft.sharepoint.spbasepermissions]::fullmask
+$dashboard.displaylevel = 'on'
+$dashboard.update()
+
+
